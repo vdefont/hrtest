27dba56e51af496c9da948670f1859cca9f1f5d5
Victor de Fontnouvelle <victordefontnouvelle@victors-mbp.lan>
2023-12-22 11:21:07 -0800

    filter

--- scrape.py
+++ scrape.py
@@ -3,14 +3,42 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
-def search(query: str) -> List[str]:
-    query_fmt = "%20".join(query.split())
-    txt = requests.get(f"https://sfbay.craigslist.org/search/bia?postedToday=1&query={query_fmt}#search=1~list~0~0").text
+"""
+TODO:
+- Debug filter links
+- Figure out how to email notif
+"""
+
+
+CL_CITIES = [["fresno", 93702], ["wichita", 67202], ["washingtondc", 20004]]
+
+def fetch_links(url: str) -> List[str]:
+    txt = requests.get(url).text
     soup = BeautifulSoup(txt, "html.parser")
     results = soup.find_all(class_="cl-static-search-result")
     links = [r.find("a").get("href") for r in results]
     return links
 
-print(search("giant"))
-print(search("surly ice cream truck"))
-print(search("surly wednesday"))
+def link_has_words(link: str, req_words: List[str]) -> List[str]:
+    txt = requests.get(link).text
+    return all(word in txt for word in req_words)
+def filter_links(links: List[str], req_words: List[str]) -> List[str]:
+
+    return [l for l in links if link_has_words(l, req_words)]
+def search(query: str, req_words: List[str]) -> List[str]:
+    """
+
+    :param query: What to search for on CL
+    :param req_words: Each of these words must appear someplace in the listing (eg. title, body)
+    :return:
+    """
+    query_fmt = "%20".join(query.split())
+    links = []
+    for city, zip in CL_CITIES:
+        url = f"https://{city}.craigslist.org/search/sss?bundleDuplicates=1&postal={zip}&postedToday=1&query={query_fmt}&search_distance=1000&sort=date#search=1~gallery~0~0"
+        links.extend(fetch_links(url))
+    return filter_links(links, req_words)
+
+
+print(search("surly ice cream truck", ["surly", "ice cream truck"]))
+print(search("surly", ["surly", "pack rat"]))
